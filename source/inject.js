6782b9da96804f16a73f1dd85f183750882fc78f
Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
2020-01-18 22:09:01 +0100

    fixed some bugs

--- source/inject.js
+++ source/inject.js
@@ -25,34 +25,49 @@ onLoadOrNavigation(function(){
 			setTimeout(function(){
 				document.head.removeChild(temp);
 			},0);
-			onStartPageLoad();
 		}else if(startPageStyles.disabled){
 			startPageStyles.disabled = false;
 		}
+		onStartPageLoad();
 	}else if(startPageStyles&&!startPageStyles.disabled){
 		startPageStyles.disabled = true;
 	}
 });
 
 async function onStartPageLoad(){
+	console.log("Running code on startpage!");
 	var colorSettings = await getColorSettings();
 	let processedVideos = [];
-	let container = document.getElementById("contents");
-	console.log("Container:",container);
-	(new MutationObserver(update)).observe(container,{childList:true});
-	update();
+	let firstMutationObserver = new MutationObserver(testForGrid);
+	firstMutationObserver.observe(document.body,{childList:true,subtree:true});
+	testForGrid();
+	function testForGrid(){
+		let container = document.querySelector("#contents.ytd-rich-grid-renderer");
+		console.log("Body Mutated! Container:",container);
+		if (container){
+			firstMutationObserver.disconnect();
+			console.log("Grid loaded");
+			(new MutationObserver(update)).observe(container,{childList:true});
+			update();
+		}
+	}
 	function update(){
-		console.log("processing new videos!");
-		let videos = document.getElementsByTagName("ytd-rich-grid-video-renderer");
-		for (let i=0;i<videos.length;i++){
-			let video = videos[i];
-			if (!processedVideos.includes(video)){
-				let title = (video.querySelector("#video-title")||{}).textContent;
-				let channel = (video.querySelector("#channel-name #container #text-container #text a")||{}).textContent;
-				console.log("Processing video: ",{title,channel});
-				videos[i].style.setProperty("--ext-yt-blocker-color",getVideoColor(colorSettings,title,channel));
-				processedVideos.push(video);
+		try {
+			console.log("processing new videos!");
+			let videos = document.getElementsByTagName("ytd-rich-grid-video-renderer");
+			console.log("videos:",videos)
+			for (let i=0;i<videos.length;i++){
+				let video = videos[i];
+				if (!processedVideos.includes(video)){
+					let title = (video.querySelector("#video-title")||{}).textContent;
+					let channel = (video.querySelector("#channel-name #container #text-container #text a")||{}).textContent;
+					console.log("Processing video: ",{title,channel});
+					videos[i].style.setProperty("--ext-yt-blocker-color",getVideoColor(colorSettings,title,channel));
+					processedVideos.push(video);
+				}
 			}
+		}catch(e){
+			console.log(e);
 		}
 	}
 }
@@ -61,8 +76,8 @@ async function onVideoPageLoad(){
 	let processedVideos = [];
 	let firstMutationObserver = new MutationObserver(testForList);
 	firstMutationObserver.observe(document.body,{childList:true});
-	testForList();
 	var colorSettings = getColorSettings();
+	testForList();
 	async function testForList(){
 		var list = document.querySelector("ytd-watch-next-secondary-results-renderer #items");
 		console.log("body mutated! list:",list);
@@ -75,35 +90,44 @@ async function onVideoPageLoad(){
 		}
 	}
 	function update(){
-		console.log("processing new videos!");
-		let videos = document.querySelectorAll("ytd-compact-video-renderer, ytd-compact-playlist-renderer");
-		for (let i=0;i<videos.length;i++){
-			let video = videos[i];
-			if (!processedVideos.includes(video)){
-				let title = video.querySelector("#video-title").textContent.trim();
-				let channel = video.querySelector("#channel-name #container #text-container #text").textContent;
-				console.log("Processing video: ",{title,channel});
-				videos[i].style.setProperty("--ext-yt-blocker-color",getVideoColor(colorSettings,title,channel));
-				processedVideos.push(video);
+		try {
+			console.log("processing new videos!");
+			let videos = document.querySelectorAll("ytd-compact-video-renderer, ytd-compact-playlist-renderer");
+			for (let i=0;i<videos.length;i++){
+				let video = videos[i];
+				if (!processedVideos.includes(video)){
+					let title = video.querySelector("#video-title").textContent.trim();
+					let channel = video.querySelector("#channel-name #container #text-container #text").textContent;
+					console.log("Processing video: ",{title,channel});
+					videos[i].style.setProperty("--ext-yt-blocker-color",getVideoColor(colorSettings,title,channel));
+					processedVideos.push(video);
+				}
 			}
+		}catch(e){
+			console.log(e);
 		}
 	}
 }
 
 async function getColorSettings(){
-	return (await browser.storage.local.get("colorSettings")).colorSettings;
+	let settings = await browser.storage.local.get("colorSettings");
+	console.log("Settings:",settings);
+	return ((settings&&settings.colorSettings)?settings.colorSettings:{default:"#efefef",categories:[]});
 }
 
 function getVideoColor(settings,title,channel){
+	console.log("Computing video color!");
 	let color = settings.default;
 	console.log("Settings:",settings);
-	for (let i=0;i<settings.categories.length;i++){
-		for (let i2=0;i2<settings.categories[i].channels.length;i2++){
-			if (channel.toUpperCase()==settings.categories[i].channels[i2].toUpperCase()){
-				color = settings.categories[i].color;
-				console.log("Found Matching channel!");
-			}else{
-				console.log("Channel not matching; channel: \""+channel+"\", other channel: \""+settings.categories[i].channels[i2]+"\"");
+	if (channel!==undefined){
+		for (let i=0;i<settings.categories.length;i++){
+			for (let i2=0;i2<settings.categories[i].channels.length;i2++){
+				if (channel.toUpperCase()==settings.categories[i].channels[i2].toUpperCase()){
+					color = settings.categories[i].color;
+					console.log("Found Matching channel!");
+				}else{
+					console.log("Channel not matching; channel: \""+channel+"\", other channel: \""+settings.categories[i].channels[i2]+"\"");
+				}
 			}
 		}
 	}
